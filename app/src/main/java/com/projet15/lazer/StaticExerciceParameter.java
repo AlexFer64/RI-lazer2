e6b331f6c15d376d2799f1c26fbe14ae57e78fc7
Unknown <nyquo.g@gmail.com>
2018-03-01 19:55:11 +0100

    Mise a jour interface statis exercice parameter amélioré

--- app/src/main/java/com/projet15/lazer/StaticExerciceParameter.java
+++ app/src/main/java/com/projet15/lazer/StaticExerciceParameter.java
@@ -44,6 +44,12 @@ public class StaticExerciceParameter extends AppCompatActivity {
         buttonStart = (Button) findViewById(R.id.START_BUTTON_ID);
 
 
+        //set les listener
+        patientName.addTextChangedListener(new textChangedListener(patientName));
+        operatorName.addTextChangedListener(new textChangedListener(operatorName));
+        markDistance.addTextChangedListener(new textChangedListener(markDistance));
+        time.addTextChangedListener(new textChangedListener(time));
+
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,6 +81,7 @@ public class StaticExerciceParameter extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "la vue suivante n'existe pas encore", Toast.LENGTH_SHORT).show();
     }
 
+    //validation PatientName
     private boolean validatePatientName() {
         //trim() enlève les espaces
         if (patientName.getText().toString().trim().isEmpty()) { //si le nom du patient est vide
@@ -127,6 +134,40 @@ public class StaticExerciceParameter extends AppCompatActivity {
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class textChangedListener implements TextWatcher {
+
+        private View view;
+
+        private textChangedListener(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2){
+
+        }
+
+        public void afterTextChanged(Editable editable){ //après qu'on est changé le texte d'un champ, on attend pas la validation pour vérifer que c'est nom vide: pour pouvoir supprimer les messsages d'erreur dès qu'on entre quelque chose
+            switch (view.getId()) {
+                case R.id.PATIENT_NAME_EDITTEXT_ID:
+                    validatePatientName();
+                    break;
+                case R.id.OPERATOR_NAME_EDITTEXT_ID:
+                    validateOperatorName();
+                    break;
+                case R.id.MARK_DISTANCE_EDITTEXT_ID:
+                    validateMarkDistance();
+                    break;
+                case R.id.TIME_EDITTEXT_ID:
+                    validateTime();
+                    break;
+            }
         }
     }
 
