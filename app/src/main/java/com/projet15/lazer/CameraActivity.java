be61decc462f7778cd82d4e086aec7f061206245
Unknown <nyquo.g@gmail.com>
2018-03-08 13:46:47 +0100

    LSDFKJMSLDKFJMSLKJFMSLDKFJMLKQFJMSLDKFj
    mfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlkmfjfqmlksdjfmqlksdjfmlk

--- app/src/main/java/com/projet15/lazer/CameraActivity.java
+++ app/src/main/java/com/projet15/lazer/CameraActivity.java
@@ -1,14 +1,22 @@
 package com.projet15.lazer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import 	android.hardware.Camera;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CameraActivity extends AppCompatActivity {
@@ -16,13 +24,17 @@ public class CameraActivity extends AppCompatActivity {
     private Parameter _parametreDeLexercice;
     private TextView _callBack;
     private Camera _camera;
-    private CameraPreview _preview;
-    private Button _button;
+    private CameraPreviewPixel _preview;
+    //private Button _button;
+    private FrameLayout displayColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        String TAG = "aaaaaaaaaaaaaaaaaaaaaaa";
+        Log.v(TAG, "index");
 
         //Recupération des données du formulaire
         _intent = getIntent();
@@ -30,7 +42,10 @@ public class CameraActivity extends AppCompatActivity {
 
         //Récupération du bouton et du texte
         _callBack = (TextView) findViewById(R.id.CALLBACK_TEXT_VIEW_ID);
-        _button = (Button) findViewById(R.id.TAKE_PICTURE_BUTTON_ID);
+        //_button = (Button) findViewById(R.id.TAKE_PICTURE_BUTTON_ID);
+        displayColor = (FrameLayout) findViewById(R.id.FRAME_LAYOUT_COLOR_ID);
+
+        //displayColor.setBackgroundColor(Color.parseColor("#FFFF0000"));
 
         //Création d'une instance de la caméra
         _camera = getCameraInstance();
@@ -43,16 +58,19 @@ public class CameraActivity extends AppCompatActivity {
             //Le mode autofocus est supporté
             Camera.Parameters ParametreCamera = _camera.getParameters(); //récuperer les paramètres de la caméra
             ParametreCamera.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO); //rajouter le mode autofocus
+            //ParametreCamera.setPictureFormat(ImageFormat.FLEX_RGB_888);
+
             _camera.setParameters(ParametreCamera); //réattribuer les nouveaux paramètres à la caméra
         }
 
         //Création de la prévisualisation et paramétrage de cette application comme contenu de du framelayout
-        _preview = new CameraPreview(this, _camera);
+        _preview = new CameraPreviewPixel(this, _camera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.CAMERA_FRAME_LAYOUT_ID);
         preview.addView(_preview);
 
 
     }
+
 
     public static Camera getCameraInstance(){
         Camera c = null;
@@ -65,4 +83,43 @@ public class CameraActivity extends AppCompatActivity {
         return c; // returns null if camera is unavailable
     }
 
+
+    /**     ----------------------------------------------------------------------------
+     *      |                                                                          |
+     *      |               TEST CAMERA PREVIEW AMELIOREE                              |
+     *      |                                                                          |
+     *      ----------------------------------------------------------------------------*/
+
+    public class CameraPreviewPixel extends CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCallback {
+        private SurfaceHolder mHolder;
+        private Camera mCamera;
+
+        //Settings de la caméra
+        private Camera.Parameters parameters;
+        //taille de la "preview" de la caméra
+        private android.hardware.Camera.Size previewSize;
+        //tableau pour stocker les pixels en paires hexadécimales
+        private int[] pixels;
+
+        public CameraPreviewPixel(Context context, Camera camera) {
+            super(context,camera);
+
+        }
+
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            parameters = camera.getParameters();
+            //transformer les pixels du formats NV21 au format RGB
+            //decodePixels(pixels, data, previewSize.width, previewSize.height);
+            //decodePixels(pixels, data, parameters.getPreviewSize().width , parameters.getPreviewSize().height);
+
+            //mettre à droite la couleur du pixel le plus en haut à droite
+
+            //displayColor.setBackgroundColor(pixels[0]);
+            _callBack.setText("format: " + parameters.getPreviewFormat());
+            //test logcat
+            String TAG = "aaaaaaaaaaaaaaaaaaaaaaa";
+            Log.v(TAG, "visiblement c'est un peu capilotracté");
+            //Log.i("Pixels", "The top right pixel has the following RGB (hexadecimal) values:" +Integer.toHexString(pixels[0]));
+        }
+    }
 }
--- app/src/main/java/com/projet15/lazer/CameraPreview.java
+++ app/src/main/java/com/projet15/lazer/CameraPreview.java
@@ -1,24 +1,38 @@
 package com.projet15.lazer;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.Log;
+import android.util.Size;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
 
 import java.io.IOException;
+import java.security.Policy;
 
 import static android.content.ContentValues.TAG;
 
 
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback
+{
     private SurfaceHolder mHolder;
     private Camera mCamera;
+
+    //Settings de la caméra
+    private Camera.Parameters parameters;
+    //taille de la "preview" de la caméra
+    private android.hardware.Camera.Size previewSize;
+    //tableau pour stocker les pixels en paires hexadécimales
+    private int[] pixels;
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
         mCamera = camera;
+
+
+
 
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
@@ -33,6 +47,12 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
+
+            //récupérer les param de la caméra
+            parameters = mCamera.getParameters();
+            previewSize = parameters.getPreviewSize();
+            pixels = new int[previewSize.width * previewSize.height]; //width sont des attributs et n'ont pas de getters
+
         } catch (IOException e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
         }
@@ -40,34 +60,69 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     public void surfaceDestroyed(SurfaceHolder holder) {
         // empty. Take care of releasing the Camera preview in your activity.
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
 
-        if (mHolder.getSurface() == null){
-            // preview surface does not exist
-            return;
-        }
-
-        // stop preview before making changes
         try {
             mCamera.stopPreview();
-        } catch (Exception e){
-            // ignore: tried to stop a non-existent preview
-        }
-
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-
-        // start preview with new settings
-        try {
-            mCamera.setPreviewDisplay(mHolder);
+            android.hardware.Camera.Size s = mCamera.getParameters().getPreviewSize();
+            mCamera.setPreviewDisplay(holder);
+            mCamera.setPreviewCallback(this);
             mCamera.startPreview();
 
-        } catch (Exception e){
-            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            //récupérer les param de la caméra
+            parameters = mCamera.getParameters();
+            previewSize = parameters.getPreviewSize();
+            Log.e("TEST WIDTH", Integer.toString(previewSize.width));
+            Log.e("TEST HEIGH", Integer.toString(previewSize.height));
+            pixels = new int[previewSize.width * previewSize.height]; //width sont des attributs et n'ont pas de getters
+        } catch (Exception ex) {
+            return;
+        }
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        //transformer les pixels du formats NV21 au format RGB
+        decodePixels(pixels, data, previewSize.width, previewSize.height);
+
+
+        //Log.i("Pixels", "The top right pixel has the following RGB (hexadecimal) values:" +Integer.toHexString(pixels[0]));
+    }
+
+    //decodePIxels
+    public static void decodePixels(int[] argb, byte[] yuv, int width, int height) { //YUV_NV21_TO_RGB
+        final int frameSize = width * height;
+
+        final int ii = 0;
+        final int ij = 0;
+        final int di = +1;
+        final int dj = +1;
+
+        int a = 0;
+        for (int i = 0, ci = ii; i < height; ++i, ci += di) {
+            for (int j = 0, cj = ij; j < width; ++j, cj += dj) {
+                int y = (0xff & ((int) yuv[ci * width + cj]));
+                int v = (0xff & ((int) yuv[frameSize + (ci >> 1) * width + (cj & ~1) + 0]));
+                int u = (0xff & ((int) yuv[frameSize + (ci >> 1) * width + (cj & ~1) + 1]));
+                y = y < 16 ? 16 : y;
+
+                int r = (int) (1.164f * (y - 16) + 1.596f * (v - 128));
+                int g = (int) (1.164f * (y - 16) - 0.813f * (v - 128) - 0.391f * (u - 128));
+                int b = (int) (1.164f * (y - 16) + 2.018f * (u - 128));
+
+                r = r < 0 ? 0 : (r > 255 ? 255 : r);
+                g = g < 0 ? 0 : (g > 255 ? 255 : g);
+                b = b < 0 ? 0 : (b > 255 ? 255 : b);
+
+                argb[a++] = 0xff000000 | (r << 16) | (g << 8) | b;
+            }
         }
     }
 }
