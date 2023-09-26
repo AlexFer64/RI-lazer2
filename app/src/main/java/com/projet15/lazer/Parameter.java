f6ebfe45ebdfb7571a4e913d1accf1dee5ff443e
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2023-09-26 08:40:14 +0200

    Création du dépot pour la retro engineering de l'ancien projet

--- app/src/main/java/com/projet15/lazer/Parameter.java
+++ app/src/main/java/com/projet15/lazer/Parameter.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 
 
 /*
-* Sauvegarde les données du formulaire
-* */
+ * Sauvegarde les données du formulaire
+ * */
 public class Parameter implements Serializable{
     //Attributs
     private int _typeExercice; //a discuter
@@ -14,26 +14,29 @@ public class Parameter implements Serializable{
     private float _markDistance;
     private  int _time;
     private float _bipIntervale; //peux etre renomée
-                                //Comment représenté le deuxieme exo
+    //Comment représenté le deuxieme exo
+    private String _commentaire;
 
 
     //Constructeur
 
-    public Parameter(int _typeExercice, String _patientName, String _operatorName, float _markDistance, int _time, float _bipIntervale) {
+    public Parameter(int _typeExercice, String _patientName, String _operatorName, float _markDistance, int _time, float _bipIntervale,String _commentaire) {
         this._typeExercice = _typeExercice;
         this._patientName = _patientName;
         this._operatorName = _operatorName;
         this._markDistance = _markDistance;
         this._time = _time;
         this._bipIntervale = _bipIntervale;
+        this._commentaire = _commentaire;
     }
 
-    public Parameter(int _typeExercice, String _patientName, String _operatorName, float _markDistance, int _time) {
+    public Parameter(int _typeExercice, String _patientName, String _operatorName, float _markDistance, int _time, String _commentaire) {
         this._typeExercice = _typeExercice;
         this._patientName = _patientName;
         this._operatorName = _operatorName;
         this._markDistance = _markDistance;
         this._time = _time;
+        this._commentaire = _commentaire;
         this._bipIntervale = -1; // A voir
     }
     public Parameter(Parameter p){
@@ -42,6 +45,7 @@ public class Parameter implements Serializable{
         set_markDistance(p.get_markDistance());
         set_time(p.get_time());
         set_bipIntervale(p.get_bipIntervale());
+        set_commentaire(p.get_commentaire());
     }
 
     //Getteur & Setteur
@@ -92,5 +96,13 @@ public class Parameter implements Serializable{
 
     public void set_bipIntervale(float _bipIntervale) {
         this._bipIntervale = _bipIntervale;
+    }
+
+    public String get_commentaire() {
+        return _commentaire;
+    }
+
+    public void set_commentaire(String _commentaire) {
+        this._commentaire = _commentaire;
     }
 }
--- app/src/main/java/com/projet15/lazer/YuvToRgb.java
+++ app/src/main/java/com/projet15/lazer/YuvToRgb.java
@@ -1,5 +1,7 @@
 package com.projet15.lazer;
 
+import android.util.Log;
+
 /**
  *
  * @author dalmau
@@ -9,6 +11,7 @@ public class  YuvToRgb {
     private int largeur, hauteur;
 
     public YuvToRgb(byte[] imageYuv, int large, int haut) {
+
         yuv420sp = imageYuv;
         largeur = large;
         hauteur = haut;
@@ -19,10 +22,10 @@ public class  YuvToRgb {
     // Cet algorithme a ete recupere sur le blog Obg1's blog
     // URL : http://obg1.wordpress.com/2010/06/26/decodeyuv420sp-using-jni-nice-performance-boost/
     public int[] decode() {
-    	int tailleImage = largeur * hauteur;
+        int tailleImage = largeur * hauteur;
         int[] rgb = new int[largeur * hauteur];
 
-    	for (int j = 0, yp = 0; j < hauteur; j++) {
+        for (int j = 0, yp = 0; j < hauteur; j++) {
             int uvp = tailleImage + (j >> 1) * largeur, u = 0, v = 0;
             for (int i = 0; i < largeur; i++, yp++) {
                 int y = (0xff & ((int) yuv420sp[yp])) - 16;
@@ -33,14 +36,14 @@ public class  YuvToRgb {
                 }
                 int y1192 = 1192 * y;
                 int r = (y1192 + 1634 * v);
-                int g = (y1192 - 833 * v - 400 * u);
-                int b = (y1192 + 2066 * u);
+                //int g = (y1192 - 833 * v - 400 * u);
+                //int b = (y1192 + 2066 * u);
                 if (r < 0) r = 0; else if (r > 262143) r = 262143;
-                if (g < 0) g = 0; else if (g > 262143) g = 262143;
-                if (b < 0) b = 0; else if (b > 262143) b = 262143;
-                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
+                //if (g < 0) g = 0; else if (g > 262143) g = 262143;
+                //if (b < 0) b = 0; else if (b > 262143) b = 262143;
+                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000);// | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
             }
-    	}
+        }
         return rgb;
     }
 
